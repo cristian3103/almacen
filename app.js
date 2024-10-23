13c6e71d2714a505347e3d1a8003aecd867800df
cristian3103 <cristiansalazar3103@gmail.com>
2024-10-22 19:41:52 -0500

    prueba

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => console.log('servidor corriendo en el puerto +{port}'));
+
+//app.listen(process.env.PORT || 3000, () => console.log("SERVIDOR FUNCIONANDO EN EL PUERTO 3000 "));
+
+app.get('/home', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
